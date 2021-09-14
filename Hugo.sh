7d3d2e668e485a2dc4aff15166ad89c0d2f94835
reinis2045 <86679720+reinis2045@users.noreply.github.com>
2021-09-14 13:51:44 +0300

    Add files via upload

--- Hugo.sh
+++ Hugo.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #git clone --recurse-submodules --depth 1 https://github.com/google/docsy.git
-cd ./
-mkdir docsy
-cd docsy
-mkdir userguide
-cd userguide
+#cd ./
+#mkdir docsy
+#cd docsy
+#mkdir userguide
+#cd userguide
 #cd docsy/userguide/
 #cd PlayerPathway
 #npm install
-git init
-git submodule add https://github.com/google/docsy.git themes/docsy
-echo 'theme = "docsy"' >> config.toml
-git submodule update --init --recursive
+#git init
+#git submodule add https://github.com/google/docsy.git themes/docsy
+#echo 'theme = "docsy"' >> config.toml
+#git submodule update --init --recursive
 #mkdir ./opt/render/project/src/PlayerPathway/public
+dir
 hugo
